fe73e44b3f8d0edba9c403e4b0fed2b1ed12b26c
AchyutKrishna-IBM <45255422+AchyutKrishna-IBM@users.noreply.github.com>
2018-11-23 17:50:47 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,16 @@
+class Even
+{
+public static void main(String args[])
+{
+int[] arr=new int[10];
+for(int i=0;i<arr.length;i++)
+{
+arr[i]=i+1;
+if (arr[i]%2==0)
+{
+System.out.println(arr[i]);
+}
+}
+}
+}
+
